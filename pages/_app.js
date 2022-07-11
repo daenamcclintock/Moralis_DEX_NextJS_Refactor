5c8074c074a17275fc9c9c1385dd439218c28f71
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-11 09:13:37 -0400

    Navbar updates, gitignore, env

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+      <NotificationProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </NotificationProvider>
+    </MoralisProvider>
+
+    
+  )
 }
 
 export default MyApp
