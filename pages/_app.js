7dbca27158a2f5fb8a5439dfa34e1b6bf7203d93
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-11 09:44:52 -0400

    added tailwind config js file

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/globals.css'
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
 
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
@@ -11,8 +14,6 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </NotificationProvider>
     </MoralisProvider>
-
-    
   )
 }
 
